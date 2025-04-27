99402cf3df45e1e7c57240363e98509078fb59ea
tatianavodovoz <vodovozt@ya.ru>
2025-04-27 20:48:42 +0300

    добавлен метод приоритизации задач Матрица Эйзенхауэра

--- server/models/task.model.ts
+++ server/models/task.model.ts
@@ -10,6 +10,8 @@ class Task extends Model {
     public task_deadline!: Date;
     public task_board_id!: number;
     public task_importance!: number;
+    public task_time_warning!: number;
+    public task_category_matrix!: number;
 
     // связи
     static associate(models: any) {
@@ -52,6 +54,16 @@ Task.init({
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false,
+    },
+    task_time_warning: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    task_category_matrix: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 4,
     },
 }, {
     sequelize,
